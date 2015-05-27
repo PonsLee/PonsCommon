5edb3666778fecae61b824d62a775d0890c27025
Pons <li_xuanan@hotmail.com>
2015-05-27 15:15:20 +0800

    add test
    add test

---
+++ PonsCommon/PonsUtilTests/CommonTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PonsUtil;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace PonsUtil.Tests
+{
+    [TestClass()]
+    public class CommonTests
+    {
+        [TestMethod()]
+        public void GetOracleInParameterWhereSqlTest()
+        {
+            string sql = PonsUtil.Common.GetOracleInParameterWhereSql("waybillno", "123,4242,13131,24242,4242", true, false);
+
+        }
+    }
+}
