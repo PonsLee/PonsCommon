c16aeec74b00089298af89f6ab6e8da00b2e1bce
Pons <li_xuanan@hotmail.com>
2015-05-30 20:37:05 +0800

    add openxml test
    add openxml test

---
+++ PonsCommon/PonsUtilTests/OpenXMLHelperTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PonsUtil;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace PonsUtil.Tests
+{
+    [TestClass()]
+    public class OpenXMLHelperTests
+    {
+        [TestMethod()]
+        public void OpenXMLHelperTest()
+        {
+            MemoryStream stream = new MemoryStream();
+            string[,] array = new string[,] {{"123"},{"4242"}};
+            using (OpenXMLHelper helper = new OpenXMLHelper(stream, OpenExcelMode.CreateNew))
+            {
+                helper.CreateNewWorksheet("信息导入模版");
+                helper.WriteData(array);
+                helper.Save();
+            }
+            stream.Seek(0, 0);
+            //return new FileStreamResult(stream, "application/vnd.ms-excel") { FileDownloadName = HttpUtility.UrlPathEncode(fileName) };
+        
+        }
+
+        [TestMethod()]
+        public void OpenXMLHelperTest1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void ReadUsedRangeToEndTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void ReadUsedRangeToEndWithoutBlankTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void CreateNewWorksheetTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void SaveTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void SetRangeFontColorTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void WriteDataTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void WriteDataTest1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void DisposeTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
