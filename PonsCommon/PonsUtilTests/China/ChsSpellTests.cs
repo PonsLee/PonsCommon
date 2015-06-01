350b5ea4cea8d9577d048908f1ab111ede55f75f
Pons <li_xuanan@hotmail.com>
2015-06-01 14:52:27 +0800

    add SpellTest
    add SpellTest

--- PonsCommon/PonsUtilTests/China/ChsSpellTests.cs
+++ PonsCommon/PonsUtilTests/China/ChsSpellTests.cs
@@ -16,6 +16,11 @@ namespace PonsUtil.China.Tests
             var name = "开封市金明站";
             var result = ChsSpell.GetChsSpell(name);
             Assert.AreEqual(result, "KFSJMZ");
+
+            //中文符号返回*
+            var chars = "k！开，封,!01";
+            var result1 = ChsSpell.GetChsSpell(chars);
+            Assert.AreEqual(result1, "k*K*F,!01");
         }
     }
 }
