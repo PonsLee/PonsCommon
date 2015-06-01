3a963ac2aa6c2d5f951fb5eacd0f7945b23d847a
Pons <li_xuanan@hotmail.com>
2015-06-01 13:54:11 +0800

    add Spell
    add Spell

---
+++ PonsCommon/PonsUtilTests/China/ChsSpellTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PonsUtil.China;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace PonsUtil.China.Tests
+{
+    [TestClass()]
+    public class ChsSpellTests
+    {
+        [TestMethod()]
+        public void GetChsSpellTest()
+        {
+            var name = "开封市金明站";
+            var result = ChsSpell.GetChsSpell(name);
+            Assert.AreEqual(result, "KFSJMZ");
+        }
+    }
+}
